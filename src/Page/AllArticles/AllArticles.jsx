373efe1c6189f20efbfa9f31b31e6ca7a59de143
mahmudul-hasan01 <mahamudul2018@gmail.com>
2023-11-26 23:28:03 +0600

    show all approved article

--- src/Page/AllArticles/AllArticles.jsx
+++ src/Page/AllArticles/AllArticles.jsx
@@ -1,7 +1,52 @@
-
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import AllArticlesCart from "./AllArticlesCart";
+import { useState } from "react";
 
 const AllArticles = () => {
-  
+
+    const [search,setSearch]=useState('')
+    const status ='Approved'
+
+    const addArticle = async () => {
+        const data = await axios.get(`http://localhost:5000/approvedArticle?status=${status}`, { withCredentials: true })
+        return data
+    }
+    const { data, refetch } = useQuery({
+        queryKey: ['approvedArticle'],
+        queryFn: addArticle
+    })
+
+    const searchData = async () => {
+        const data = await axios.get(`http://localhost:5000/approved?search=${search}`, { withCredentials: true })
+        return data
+    }
+    const { data:searchInfo } = useQuery({
+        queryKey: ['searchData'],
+        queryFn: searchData
+    })
+
+
+
+    const handleSearch=(e)=>{
+        e.preventDefault()
+        const search =e.target.search.value
+        setSearch(search)
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSearch}>
+                <input name="search" type="text" />
+                <button className="py-2 text-white px-4 rounded-r-lg bg-blue-600">search</button>
+            </form>
+        <div className="grid grid-cols-2 gap-5">
+            {
+                data?.data.map(item => <AllArticlesCart key={item._id} item={item}></AllArticlesCart>)
+            }
+        </div>
+        </div>
+    );
 };
 
 export default AllArticles;
---
+++ src/Page/AllArticles/AllArticlesCart.jsx
@@ -0,0 +1,18 @@
+
+const AllArticlesCart = ({item}) => {
+    const { _id, title, image, publisher, description, tags, name, photo, email, status, premium, date } = item
+    return (
+        <div>
+            <div className="flex flex-col h-[300px] items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <img className="object-cover w-[220px]  rounded-t-lg h-full md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={image} alt="" />
+                    <div className="flex flex-col justify-between p-4 leading-normal">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default AllArticlesCart;
